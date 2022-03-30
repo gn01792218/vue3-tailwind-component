bb204e246a9ee45cb3c2ad9f114273901580c4f0
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-03-30 12:53:53 +0800

    建立組件資料夾

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,6 @@
 <template>
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p class="text-white">See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button text-white" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1 class="text-slot-400 text-2xl">組件展示</h1>
+  
 </template>
 <script setup lang="ts">
 import { ref } from "vue";

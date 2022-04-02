4ae3fdc74850d3b00f9eff3daf4ab0f2f2f6db69
MSI\gn01792218 <gn01792218@hotmail.com>
2022-04-02 10:59:04 +0800

    更改展示頁的排版方式

--- src/component/Nav.vue
+++ src/component/Nav.vue
@@ -1,11 +1,8 @@
 <template>
-  <nav class="w-full flex justify-center">
-      <router-link class="mr-5 btn" to="/CheckBoxDemo">checkBox組件</router-link>
-      <router-link class="mr-5 btn" to="/KnobDemo">knob組件</router-link>
+  <nav class="w-full flex justify-center mb-5">
+    <a class="text-2xl hover:text-orange-400 mr-5" href="https://github.com/gn01792218/vue3-tailwind-component" target="_blank"><i class="bi bi-github"></i>gitHub</a>
+    <router-link to="/" class="mr-5 text-2xl hover:text-green-600"><i class="bi bi-house-door"></i>返回首頁</router-link>
   </nav>
 </template>
 <script setup lang="ts">
-
-
-
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="text-center">
-  <a class="text-xl hover:text-orange-400" href="https://github.com/gn01792218/vue3-tailwind-component" target="_blank"><i class="bi bi-github"></i>gitHub</a>
   <h1 class="text-slot-400 text-2xl mb-10">Vue3+Typscript+Tailwind自製組件展示</h1>
-  <Nav/>
+  <router-link class="mr-5 btn" to="/CheckBoxDemo">checkBox組件</router-link>
+    <router-link class="mr-5 btn" to="/KnobDemo">knob組件</router-link>
 </div>
   
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-import Nav from '@/component/Nav.vue'
+
 </script>

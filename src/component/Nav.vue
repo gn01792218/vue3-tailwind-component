09f15a4cefd98e59eab82c34c91141eaa64d3665
MSI\gn01792218 <gn01792218@hotmail.com>
2022-05-22 19:17:29 +0800

    調整首頁的RWD

--- src/component/Nav.vue
+++ src/component/Nav.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="w-full flex justify-center mb-5">
-    <a class="text-2xl hover:text-orange-400 mr-5" href="https://github.com/gn01792218/vue3-tailwind-component" target="_blank"><i class="bi bi-github"></i>gitHub</a>
-    <router-link to="/" class="mr-5 text-2xl hover:text-green-600"><i class="bi bi-house-door"></i>返回首頁</router-link>
+  <nav class="w-full flex justify-center mb-5 md:text-xl sm:text-sm">
+    <a class="hover:text-orange-400 mr-5" href="https://github.com/gn01792218/vue3-tailwind-component" target="_blank"><i class="bi bi-github"></i>gitHub</a>
+    <router-link to="/" class="mr-5 hover:text-green-600"><i class="bi bi-house-door"></i>返回首頁</router-link>
   </nav>
 </template>
 <script setup lang="ts">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-<div class="text-center">
-  <h1 class="text-slot-400 text-2xl mb-10">Vue3+Typscript+Tailwind自製組件展示</h1>
-  <router-link class="mr-5 btn" to="/CheckBoxDemo">checkBox組件</router-link>
-  <router-link class="mr-5 btn" to="/KnobDemo">knob組件</router-link>
-  <router-link class="mr-5 btn" to="/BtnDemo">btn組件</router-link>
-</div>
-  
+  <div class="text-center">
+    <h1 class="text-slot-400 mb-10 md:text-2xl sm:text-xl">
+      Vue3+Typscript+Tailwind自製組件展示
+    </h1>
+    <div class="flex flex-wrap justify-center">
+      <router-link class="mr-5 btn" to="/CheckBoxDemo"
+        >checkBox組件</router-link
+      >
+      <router-link class="mr-5 btn" to="/KnobDemo">knob組件</router-link>
+      <router-link class="mr-5 btn" to="/BtnDemo">btn組件</router-link>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-
 </script>

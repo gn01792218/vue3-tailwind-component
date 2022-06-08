f8ef2b1a2f11cdb91c6eb2c32050b5ad78b8f22a
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-06-08 14:04:24 +0800

    新增card2樣式

--- src/views/CardsDemo.vue
+++ src/views/CardsDemo.vue
@@ -12,6 +12,16 @@
             }"
         />
       </DemoSlot>
+       <DemoSlot
+        title="card樣式2"
+      >
+        <Card2
+            :card="{
+                caption:'This is a Picture!',
+                description:'add some description to card now~!'
+            }"
+        />
+      </DemoSlot>
   </div>
 </div>
   
@@ -19,5 +29,5 @@
 <script setup lang="ts">
 import DemoSlot from '../component/DemoSlot.vue'
 import Card1 from '../component/cards/Card1.vue'
-
+import Card2 from '../component/cards/card2.vue'
 </script>

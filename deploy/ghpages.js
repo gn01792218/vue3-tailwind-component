04b4ebc16f35a9f043afe73e03d4176d7d304464
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-06-28 15:07:24 +0800

    新增一鍵部署github功能

---
+++ deploy/ghpages.js
@@ -0,0 +1,19 @@
+//deploy/ghpages.js
+const path = require('path');
+const ghpages = require('gh-pages');
+
+const options = {
+    branch: 'gh-pages',
+    repo: 'https://github.com/gn01792218/vue3-tailwind-component.git' // project github repo
+};
+
+const callback = err => {
+
+    if (err) console.error(err);
+    else console.log('publish success');
+};
+
+/**
+ * This task pushes to the `master` branch of the configured `repo`.
+ */
+ghpages.publish(path.resolve(__dirname, '../dist'), options, callback);
